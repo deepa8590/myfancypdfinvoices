861e40fc419aef9b4e6d90826249dcc8b695b1b5
Deepa Kumari <ppdeepar@gmail.com>
2026-09-01 22:58:09 +0530

    @PropertySources & @Value

--- src/main/java/com/marcobehler/myfancypdfinvoices/context/MyFancyPdfInvoicesApplicationConfiguration.java
+++ src/main/java/com/marcobehler/myfancypdfinvoices/context/MyFancyPdfInvoicesApplicationConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.Scope;
 
 import com.marcobehler.ApplicationLauncher;
@@ -14,11 +15,12 @@ import tools.jackson.databind.ObjectMapper;
 
 @Configuration
 @ComponentScan(basePackageClasses = ApplicationLauncher.class)
+@PropertySource("classpath:application.properties")
 public class MyFancyPdfInvoicesApplicationConfiguration {
-    // @Bean
-    public InvoiceService invoiceService() {
-        return new InvoiceService(userService());
-    }
+    // // @Bean
+    // public InvoiceService invoiceService() {
+    //     return new InvoiceService(userService(), cdnUrl());
+    // }
 
     // @Bean
     @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
--- src/main/java/com/marcobehler/myfancypdfinvoices/service/InvoiceService.java
+++ src/main/java/com/marcobehler/myfancypdfinvoices/service/InvoiceService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.marcobehler.myfancypdfinvoices.model.Invoice;
@@ -17,6 +18,10 @@ public class InvoiceService {
     //CopyOnWriteArrayList is a thread-safe variant of ArrayList(Whereas ArrayList wuld not be ) in which all mutative operations (add, set, and so on) are implemented by making a fresh copy of the underlying array.
     //use when read opr are more frequent than write operations, and when you need to iterate over the list without worrying about concurrent modifications.
     List<Invoice> invoices = new CopyOnWriteArrayList<>(); // 
+
+    private final UserService userService;
+    private final String cdnUrl;
+
     
     @PostConstruct
     public void init(){
@@ -28,11 +33,11 @@ public class InvoiceService {
     public void shutdown(){
         System.out.println("InvoiceService destroyed");
     }
-    private final UserService userService;
 
     @Autowired
-    public InvoiceService(UserService userService) {
+    public InvoiceService(UserService userService , @Value("${cdn.url}") String cdnUrl) {
         this.userService = userService;
+        this.cdnUrl = cdnUrl;
     }
 
     public List<Invoice> findAll() {
@@ -50,7 +55,7 @@ public class InvoiceService {
 
 
         // TODO real pdf creation and storing it on network server
-        Invoice invoice = new Invoice(userId, amount, "http://www.africau.edu/images/default/sample.pdf");
+        Invoice invoice = new Invoice(userId, amount, cdnUrl + "/images/default/sample.pdf");
         invoices.add(invoice);
         return invoice;
     }
